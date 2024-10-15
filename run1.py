23f1bf0869511ef50837067c1ae0d123204b1b00
Sahil <sahil.a.thadani@gmai.com>
2024-10-15 19:18:48 -0400

    Run one with boat works

--- run1.py
+++ run1.py
@@ -10,8 +10,17 @@ import move_functions
 
 
 def run1 (ev3, walter, attach_left_motor, attach_right_motor):
-    walter.straight(315)
-    walter.turn(-45)
-    walter.straight(220)
-    walter_run_for_seconds(right_motor, left_motor, 30, 1)
-    walter.turn(70)
+#unexpected encounter
+    walter.straight(300)
+    walter.turn(-55)
+    walter.straight(175)
+    walter.straight(-165)
+    walter.turn(50)
+#krill_numero_uno
+    walter.straight(75)
+    walter.turn(40)
+#change_shipping_lanes
+    attach_left_motor.run_time(-700, 700, Stop.COAST, wait=True)
+    walter.straight(50)
+    attach_left_motor.run_angle(300,50, Stop.HOLD, wait=True)
+    walter.turn(45)
