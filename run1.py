1538fb406da760ab68e536a0e970d92b643fb0d6
Bat-Chen <bzeliger@gmail.com>
2024-11-09 14:20:41 -0500

    run1 with no bucket

--- run1.py
+++ run1.py
@@ -6,58 +6,54 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from move_functions import *
+
 from gyro_turn import gyro_turn
 
 
 
 def run1 (ev3, walter, left_motor, right_motor, attach_left_motor, attach_right_motor, gyro):
 #unexpected encounter
+    walter.settings(600, 400, 300, 115)
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
     walter.straight(280)
-    gyro_turn(-51, left_motor, right_motor, gyro, walter)
-    walter.straight(240)
+    gyro_turn(-50, left_motor, right_motor, gyro, walter)
+    walter.straight(270)
     walter.straight(-165)
-    gyro_turn(85, left_motor, right_motor, gyro, walter)
+    gyro_turn(92.5, left_motor, right_motor, gyro, walter)
 #change_shipping_lanes
     attach_left_motor.run_time(-700, 700, Stop.COAST, wait=True)
-    walter.straight(115)
+    walter.straight(95)
     attach_left_motor.run_angle(300, 35, Stop.HOLD, True)
     wait(1000)
-    walter.stop()
     gyro_turn(20, left_motor, right_motor, gyro, walter)
-    walter.straight(-50)
+    walter.stop()
+    walter.straight(-60)
+    wait(1000)
     attach_left_motor.run_angle(75, -10, Stop.COAST, wait=True)
     ev3.speaker.beep(500,200)
     walter.straight(-50)
     attach_left_motor.run_angle(300, 180, Stop.COAST, wait=True)
-#get_the_krill
-    walter.turn(-55)
-    walter.straight(400)
-    gyro_turn(-16.99, left_motor, right_motor, gyro, walter)
-    walter.straight(400)
-    # walter.straight(-180)
-    # walter.turn(-60)
-    # walter.straight)
-    # walter.turn(-70)
-    # walter.straight(100)
-    # wait(1000)
-    # claw_down(left_motor, 100, 180)
-    # wait(1000)
-    # walter.straight(-200)
-
-#radar    
-    # walter.turn(25)
-    # wait(3000)
-    # walter.straight(100)
-    # attach_right_motor.run_angle(300, 80, Stop.COAST, wait=TRUE)
+# move towards radar
+    walter.turn(-65)
+    walter.straight(475)
+    walter.turn(-51)
+    walter.straight(30)
+# do the radar
+    claw_down(attach_left_motor, 350, 200)
+    walter.straight(-200)
+# green sample mission
+    walter.stop()
+    claw_up(attach_left_motor, 300, 35)
+    wait(2000)
+    walter.reset()
+    walter.drive(-80, 90)
+    while (walter.distance() > -160) and (walter.angle() < 180):
+        wait(1)
+    walter.straight(-50)
+    walter.stop()
     
-   
+    # right_motor.run_angle(200, -395)
+    # walter.straight(-20)
+    # claw_down(attach_left_motor, 350, 20)
     
-    
-    #walter.straight(-10)
-    #walter.turn(165)
-    #walter.straight(100)
-    #walter.straight(-200)
-    #walter.turn(85)
-    #walter.straight(350)
