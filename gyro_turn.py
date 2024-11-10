c06327afeced935291c4ef78ffefd634ebf43dac
Sahiltheram <sahil.a.thadani@gmail.com>
2024-11-10 09:06:41 -0500

    gyro changes

--- gyro_turn.py
+++ gyro_turn.py
@@ -6,28 +6,30 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 
 def set_speed(error, threshold_test, maxspeed, minspeed):
-    speed_set = (((error / threshold_test) * (maxspeed - minspeed)) + minspeed)
+    if (error < 0):
+        speed_set = (((error / threshold_test) * (maxspeed - minspeed)) - minspeed)
+    else: 
+        speed_set = (((error / threshold_test) * (maxspeed - minspeed)) + minspeed)
     return (speed_set)
 
 def gyro_turn(angle, left_motor, right_motor, gyro, walter):
     MAX_SPEED = 300
     MIN_SPEED = 30
-    threshold = abs(angle)
     error_threshold = 2
-    direction = 1
-    if (angle < 0):
-        direction = -1
-
+    angle_for_max_speed = 40
     gyro.reset_angle(0)
     walter.stop()
     while (True): 
-        rem_dist = abs(angle - gyro.angle())
-        speed = max(set_speed(rem_dist, threshold, MAX_SPEED, MIN_SPEED), MIN_SPEED)
-        left_motor.run(speed * direction)
-        right_motor.run(-speed * direction)
-        wait(15)
+        rem_dist = angle - gyro.angle()
+        if (abs(rem_dist) > angle_for_max_speed):
+            speed = MAX_SPEED
+        else: 
+            speed = set_speed(rem_dist, angle, MAX_SPEED, MIN_SPEED)
+            left_motor.run(speed)
+            right_motor.run(-speed)
+            wait(15)
         #print(speed, rem_dist)
-        if rem_dist < error_threshold:
+        if abs(rem_dist) < error_threshold:
             break
     left_motor.stop()
     right_motor.stop()
