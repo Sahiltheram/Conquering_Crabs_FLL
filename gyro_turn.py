fe2c33001f69963d4f3913efa881945a8d55aa2c
Bat-Chen <bzeliger@gmail.com>
2024-11-11 10:32:30 -0500

    fix gyro turn

--- gyro_turn.py
+++ gyro_turn.py
@@ -5,32 +5,46 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 
-def set_speed(error, threshold_test, maxspeed, minspeed):
+def set_speed(error, threshold_test, minspeed, maxspeed):
     if (error < 0):
-        speed_set = (((error / threshold_test) * (maxspeed - minspeed)) - minspeed)
+        speed_set = ((error / threshold_test) * maxspeed)
     else: 
-        speed_set = (((error / threshold_test) * (maxspeed - minspeed)) + minspeed)
+        speed_set = ((error / threshold_test) * maxspeed)
     return (speed_set)
 
 def gyro_turn(angle, left_motor, right_motor, gyro, walter):
-    MAX_SPEED = 300
+    MAX_SPEED = 400
     MIN_SPEED = 30
-    error_threshold = 2
-    angle_for_max_speed = 40
+    ERROR_THRESHOLD = 2
+    ANGLE_FOR_MAX_SPEED = 70
+
     gyro.reset_angle(0)
     walter.stop()
-    while (True): 
+    while (True):
         rem_dist = angle - gyro.angle()
-        if (abs(rem_dist) > angle_for_max_speed):
+        if rem_dist > ANGLE_FOR_MAX_SPEED:
             speed = MAX_SPEED
-        else: 
-            speed = set_speed(rem_dist, angle, MAX_SPEED, MIN_SPEED)
-            left_motor.run(speed)
-            right_motor.run(-speed)
-            wait(15)
-        #print(speed, rem_dist)
-        if abs(rem_dist) < error_threshold:
+        elif rem_dist < -ANGLE_FOR_MAX_SPEED:
+            speed = -MAX_SPEED
+        else:
+           speed = set_speed(rem_dist, ANGLE_FOR_MAX_SPEED, MIN_SPEED, MAX_SPEED)
+           #print("error="+str(rem_dist)+", calc speed="+str(speed))
+           if speed < 0:
+               if abs(speed) < MIN_SPEED:
+                  speed = -MIN_SPEED
+               if abs(speed) > MAX_SPEED:
+                  speed = -MAX_SPEED
+           if (speed > 0):
+               if abs(speed) < MIN_SPEED:
+                  speed = MIN_SPEED
+               if abs(speed) > MAX_SPEED:
+                  speed = MAX_SPEED
+        left_motor.run(speed)
+        right_motor.run(-speed)
+        wait(15)
+        if abs(rem_dist) < ERROR_THRESHOLD:
             break
     left_motor.stop()
     right_motor.stop()
-    print(speed, rem_dist)
+
+    print("DONE, angle="+str(gyro.angle()))
