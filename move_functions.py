748be62357dca0006d42cb3573d2374f50699faa
Bat-Chen <bzeliger@gmail.com>
2024-12-05 18:46:40 -0500

    added documentation

--- move_functions.py
+++ move_functions.py
@@ -7,12 +7,12 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 timer = StopWatch()
-
+#this is used to allign on walls, mission, and for moving one motor for seconds
 def walter_run_for_seconds(robot, left_speed, right_speed, seconds):
     robot.walter.stop()
     robot.left_motor.run_time(left_speed, seconds * 1000, Stop.HOLD, False)
     robot.right_motor.run_time(right_speed, seconds * 1000, Stop.HOLD, True)
-
+#use for moving claw up and down
 def claw_up(speed, angle, robot):
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_angle(speed, angle)
@@ -21,6 +21,7 @@ def claw_down(speed, angle, robot):
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_angle(speed, -angle)
 
+#use for moving the claw up and down while keeping it in coast
 def claw_up_coast(speed, angle, robot):
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_angle(speed, angle, Stop.COAST)
@@ -29,6 +30,7 @@ def claw_down_coast(speed, angle, robot):
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_angle(speed, -angle, Stop.COAST)
 
+#use for moving the stick up and down
 def stick_up(speed, angle, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_angle(speed, angle)
@@ -37,6 +39,7 @@ def stick_down(speed, angle, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_angle(speed, -angle)
 
+#use for moving stick up and down in coast
 def stick_up_coast(speed, angle, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_angle(speed, angle, Stop.COAST)
@@ -45,6 +48,7 @@ def stick_down_coast(speed, angle, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_angle(speed, -angle, Stop.COAST)
 
+#use for resetting the stick
 def stick_down_stalled(speed, dutylimit, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_until_stalled(-speed, Stop.HOLD, duty_limit = dutylimit)
@@ -53,6 +57,7 @@ def stick_up_stalled(speed, dutylimit, robot):
     robot.attach_right_motor.reset_angle(0)
     robot.attach_right_motor.run_until_stalled(speed, Stop.HOLD, duty_limit = dutylimit)
 
+#use for color alligning(on white)
 def walter_align_color(robot, left_speed, right_speed):
     robot.left_motor.run(left_speed)
     robot.right_motor.run(right_speed)
@@ -70,17 +75,19 @@ def walter_align_color(robot, left_speed, right_speed):
         if (is_stopped_left) and (is_stopped_right):
             break
 
-
+#use for resetting claw
 def claw_up_stalled(speed, dutylimit, robot):    
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_until_stalled(speed, Stop.HOLD, duty_limit = dutylimit)
     robot.attach_left_motor.reset_angle(0)
+
 
 def claw_down_stalled(speed, dutylimit, robot):    
     robot.attach_left_motor.reset_angle(0)
     robot.attach_left_motor.run_until_stalled(-speed, Stop.HOLD, duty_limit = dutylimit)
     robot.attach_left_motor.reset_angle(0)
 
+#use for moving claw when you dont need to be as accurate
 def claw_down_seconds(speed, seconds, robot):
     robot.attach_left_motor.run_time(-speed, seconds)
     robot.attach_left_motor.reset_angle(0)
@@ -89,6 +96,7 @@ def claw_up_seconds(speed, seconds, robot):
     robot.attach_left_motor.run_time(speed, seconds)
     robot.attach_left_motor.reset_angle(0)
 
+#use for moving stick when you dont need to be as accurate
 def stick_down_seconds(speed, seconds, robot):
     robot.attach_right_motor.run_time(-speed, seconds)
     robot.attach_right_motor.reset_angle(0)
@@ -97,6 +105,7 @@ def stick_up_seconds(speed, seconds, robot):
     robot.attach_right_motor.run_time(speed, seconds)
     robot.attach_right_motor.reset_angle(0)
 
+#use for moving claw up and down when it might get stuck
 def claw_down_timeout(speed, angle, timeout_seconds, robot):
     timeout_ms = timeout_seconds * 1000
     robot.attach_left_motor.reset_angle(0)
@@ -117,6 +126,7 @@ def claw_up_timeout(speed, angle, timeout_seconds, robot):
             break
     robot.attach_left_motor.hold()
 
+#use for moving stick up and down when it might get stuck
 def stick_down_timeout(speed, angle, timeout_seconds, robot):
     timeout_ms = timeout_seconds * 1000
     robot.attach_right_motor.reset_angle(0)
@@ -136,6 +146,3 @@ def stick_up_timeout(speed, angle, timeout_seconds, robot):
         if (robot.attach_right_motor.angle() > angle) or (timer.time() > timeout_ms):
             break
     robot.attach_right_motor.hold()
-
-
-
--- run5.py
+++ run5.py
@@ -5,7 +5,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import move_functions
+from move_functions import *
 from gyro_turn import *
 from Container import Container
 
@@ -16,44 +16,50 @@ from Container import Container
  
 def run5 (robot):
     robot.walter.settings(400, 350, 200, 200)
-    robot.walter.straight(180)
+    #put samples on boat
+    robot.walter.straight(210)
     robot.walter.straight(-90)
     robot.walter.stop()
-    robot.right_motor.run_angle(200, 99, Stop.HOLD, wait=True)
-    gyro_turn(-70, robot)
-    robot.walter.straight(-100)
+    gyro_turn(-93, robot)
+    #allign on wall
+    walter_run_for_seconds(robot, -500, -500, 1)
     robot.walter.straight(246)
     gyro_turn(92, robot)
-    robot.walter.straight(327)
-    gyro_turn(31, robot)
+    robot.walter.straight(353)
+    gyro_turn(25, robot)
     wait(500)
-    robot.attach_right_motor.run_angle(400, -120, Stop.HOLD, wait=True)
-    robot.attach_right_motor.run_until_stalled(-183, Stop.HOLD, duty_limit=70)
+    robot.walter.straight(-15)
+    #hit boat most of the way
+    stick_down(400, 120, robot)
+    #hit boat the rest of the way
+    stick_down_timeout(183, 70, 0.8, robot)
     robot.attach_right_motor.reset_angle(0)
-    robot.attach_right_motor.run_angle(50, 5, Stop.HOLD, wait=True)
-    robot.walter.straight(450)
-    robot.attach_right_motor.run_angle(700, 170, Stop.HOLD, wait=True)
+    stick_up(50, 5, robot)
+    #push the boat half way
+    robot.walter.straight(460)
+    stick_up(700,170, robot)
+    #adjust placement to push the boat from behind
     robot.walter.straight(-75)
     robot.walter.turn(30)
-    robot.walter.straight(345)
-    # gyro_turn(-90, left_motor, right_motor, gyro, walter)
-    # walter.straight(-160)
-    # gyro_turn(90, left_motor, right_motor, gyro, walter)
-    # walter.straight(400)
-    robot.walter.straight(-200)
+    robot.walter.straight(370)
+    walter_run_for_seconds(robot, -500, -500, 1)
+    #allign on wall
     gyro_turn(-90, robot)
     robot.walter.straight(-80)
+    #set up for crab tower
     robot.walter.straight(183)
     gyro_turn(90, robot)
     #Crabs
     robot.walter.straight(-20)
-    robot.attach_right_motor.run_angle(400, -149, Stop.HOLD)
+    stick_down(400, -149, robot)
     robot.walter.straight(63)
+    #raise tower
     robot.attach_right_motor.run_angle(125, 130, Stop.COAST, False)
     robot.walter.straight(-192)
     robot.attach_right_motor.run_until_stalled(125, Stop.COAST, duty_limit=70)
     robot.walter.straight(-75)
     robot.attach_right_motor.run_until_stalled(130, Stop.COAST, duty_limit=70)
+    #go to right home area
     robot.walter.turn(-20)
     robot.walter.straight(300)
     robot.walter.turn(20)
